171b62c173c13f5dbafa3e37391547f8241d4be9
Cody Swartz <ctsstc@gmail.com>
2021-05-31 12:42:40 -0700

    Finish: Triangle Project

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+
+  return :equilateral if (a == b && b == c)
+
+  return :isosceles if
+    (a == b && b != c) ||
+    (b == c && a != c) ||
+    (a == c && b != a)
+
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
